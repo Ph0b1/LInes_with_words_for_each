b4127e00edd9f2804b970e654ba352c8669b49b4
Max Strijak <maxphobos69@gmail.com>
2019-06-06 00:09:17 +0300

    Update

--- src/com/company/AlphabeticallySortLinesOfTextInFile.java
+++ src/com/company/AlphabeticallySortLinesOfTextInFile.java
@@ -8,30 +8,30 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-public class AlphabeticallySortLinesOfTextInFile {
+public class AlphabeticallySortLinesOfTextInFile { // creating public class
 
     public static void main(String[] args) throws Exception {
 
-        String inputFile = "input.txt";
-        String outputFile = "output.txt";
+        String inputFile = "input.txt"; //Set a string for a input from read file.
+        String outputFile = "output.txt"; // Set a string for output file created.
 
-        FileReader fileReader = new FileReader(inputFile);
-        BufferedReader bufferedReader = new BufferedReader(fileReader);
-        String inputLine;
+        FileReader fileReader = new FileReader(inputFile); // Setting where the data will be taken from.
+        BufferedReader bufferedReader = new BufferedReader(fileReader); // Reading an input file.
+        String inputLine; // Initializing a string for read lines.
         List<String> lineList = new ArrayList<String>();
-        while ((inputLine = bufferedReader.readLine()) != null) {
+        while ((inputLine = bufferedReader.readLine()) != null) { // reading file till the end.
             lineList.add(inputLine);
         }
-        fileReader.close();
+        fileReader.close(); // Close the read file and saving resources.
 
-        Collections.sort(lineList);
+        Collections.sort(lineList); // Sorting all lines in alphabetical order.
 
-        FileWriter fileWriter = new FileWriter(outputFile);
+        FileWriter fileWriter = new FileWriter(outputFile); // Spesifing the output file where sorted lines will be inputted.
         PrintWriter out = new PrintWriter(fileWriter);
         for (String outputLine : lineList) {
             out.println(outputLine);
         }
-        out.flush();
+        out.flush(); // 
         out.close();
         fileWriter.close();
 
