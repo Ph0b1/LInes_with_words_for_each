659a09a9bd0785781954550a705bf96ba5e86131
Max Strijak <maxphobos69@gmail.com>
2019-06-08 00:38:06 +0300

    Update

--- src/com/company/AlphabeticallySortLinesOfTextInFile.java
+++ src/com/company/AlphabeticallySortLinesOfTextInFile.java
@@ -1,39 +1,58 @@
 package fileRead;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.PrintWriter;
-import java.util.ArrayList;
+import java.rmi.server.ObjID;
+import java.util.Comparator;
+import java.util.LinkedList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Arrays;
+
+import static java.util.Collections.*;
 
 public class AlphabeticallySortLinesOfTextInFile { // creating public class
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
 
-        String inputFile = "input.txt"; //Set a string for a input from read file.
-        String outputFile = "output.txt"; // Set a string for output file created.
+        List<String> line1 = new LinkedList<String>();
+        line1.add(0, "How");
+        line1.add(1, "doth");
+        line1.add(2, "the");
+        line1.add(3, "little");
+        line1.add(4, "crocodile");
 
-        FileReader fileReader = new FileReader(inputFile); // Setting where the data will be taken from.
-        BufferedReader bufferedReader = new BufferedReader(fileReader); // Reading an input file.
-        String inputLine; // Initializing a string for read lines.
-        List<String> lineList = new ArrayList<String>();
-        while ((inputLine = bufferedReader.readLine()) != null) { // reading file till the end.
-            lineList.add(inputLine);
+        List<String> line2 = new LinkedList<String>();
+        line2.add(0, "Improve");
+        line2.add(1, "his");
+        line2.add(2, "shining");
+        line2.add(3, "tail");
+
+        List<String> line3 = new LinkedList<String>();
+        line3.add(0, "And");
+        line3.add(1, "pour");
+        line3.add(2, "the");
+        line3.add(3, "waters");
+        line3.add(4, "of");
+        line3.add(5, "the");
+        line3.add(6, "Nile");
+
+        List<String> line4 = new LinkedList<String>();
+        line4.add(0, "On");
+        line4.add(1, "every");
+        line4.add(2, "golden");
+        line4.add(3, "scale!");
+
+
+        System.out.print(line1 + "\n" + line2 + "\n" + line3 + "\n" + line4);
+        System.out.printf("\n");
+        
+        String[] test1 = new String[]{"doth", "his", "pour", "every"};
+
+        Arrays.sort(test1, String.CASE_INSENSITIVE_ORDER);
+
+        for (int a = 0; a < test1.length; a++) {
+            System.out.println(test1[a]);
+
+
         }
-        fileReader.close(); // Close the read file and saving resources.
-
-        Collections.sort(lineList); // Sorting all lines in alphabetical order.
-
-        FileWriter fileWriter = new FileWriter(outputFile); // Spesifing the output file where sorted lines will be inputted.
-        PrintWriter out = new PrintWriter(fileWriter);
-        for (String outputLine : lineList) {
-            out.println(outputLine);
-        }
-        out.flush(); // 
-        out.close();
-        fileWriter.close();
-
     }
 }
